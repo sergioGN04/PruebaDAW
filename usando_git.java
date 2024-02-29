5dc89734c832075a4faea248e848e6cef48fc28a
sergioGN04 <sergio.gonzalez.noria.alu@iesfernandoaguilar.es>
2024-02-29 15:51:24 +0100

    commit del .gitignore

--- usando_git.java
+++ usando_git.java
@@ -2,5 +2,6 @@ public class usando_git {
 	public static void main(String[] args){
 		System.out.println("Proyecto con GIT");
 		System.out.println("Modificacion a la clase");
+		System.out.println("gagse");
 	}
 }
